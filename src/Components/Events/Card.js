e37524e7e0ebdb75c151110058359c2d6819f4cc
Sebastian Porumb <sebyporumb@gmail.com>
2024-02-13 18:30:44 +0200

    date before location

--- src/Components/Events/Card.js
+++ src/Components/Events/Card.js
@@ -6,9 +6,9 @@ const Card = ({ title, date, location, city, image }) => {
     <div className="card">
       <div className="card-content">
         <p className="card-title">{title}</p>
+        <p className="card-date">Date: {date}</p>
         <p className="card-location">{location}</p>
         <p className="card-city">{city}</p>
-        <p className="card-date">Date: {date}</p>
       </div>
       <img className="card-image" src={image} alt={title} />
     </div>
