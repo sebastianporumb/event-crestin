8e2a826d6db111eaf80a191465297129803253ee
Sebastian Porumb <sebyporumb@gmail.com>
2024-02-15 18:18:23 +0200

    no location on card

--- src/Components/Events/Card.js
+++ src/Components/Events/Card.js
@@ -10,9 +10,8 @@ const Card = ({ id, title, date, location, city, image, onClick }) => {
     <div className="card" onClick={handleClick}>
       <div className="card-content">
         <p className="card-title">{title}</p>
-        <p className="card-date">{date}</p>
-        <p className="card-location">{location}</p>
         <p className="card-city">{city}</p>
+        <p className="card-date">{date}</p>
       </div>
       <img className="card-image" src={image} alt={title} />
     </div>
