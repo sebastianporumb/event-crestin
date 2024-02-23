9f236fb5fb806b5f3401ee4adde9d900c78b5581
Sebastian Porumb <sebyporumb@gmail.com>
2024-02-23 17:30:13 +0200

    add popup

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./App.css";
 import { EventsListData } from "./Components/Events/EventsList";
 import EventsDescription from "./Components/Events/EventsDescription";
 import Card from "./Components/Events/Card";
+import EventPopup from "./Components/Events/EventPopup";
 
 function App() {
   const [selectedCard, setSelectedCard] = useState(null);
   const [selectedEventType, setSelectedEventType] = useState("all");
   const [searchText, setSearchText] = useState("");
-  const main2ContainerRef = useRef(null);
 
   const onEventClick = (eventId) => {
-    if (main2ContainerRef.current) {
-      main2ContainerRef.current.scrollTop = 0;
-    }
-
     setSelectedCard(eventId);
+  };
+
+  const closePopup = () => {
+    setSelectedCard(null);
   };
 
   const handleEventTypeClick = (eventType) => {
@@ -47,82 +47,72 @@ function App() {
         );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="topnav">
-          <a
-            className={selectedEventType === "all" ? "active" : ""}
-            onClick={() => handleEventTypeClick("all")}
-            href="#home"
-          >
-            Home
-          </a>
-          <a
-            className={selectedEventType === "concert" ? "active" : ""}
-            onClick={() => handleEventTypeClick("concert")}
-            href="#concert"
-          >
-            Concerte
-          </a>
-          <a
-            className={selectedEventType === "conferinta" ? "active" : ""}
-            onClick={() => handleEventTypeClick("conferinta")}
-            href="#conferinta"
-          >
-            Conferinte
-          </a>
-          <div class="search-container">
-            <form action="/action_page.php">
-              <input
-                type="text"
-                placeholder="Titlu, oras"
-                name="search"
-                value={searchText}
-                onChange={handleSearch}
-              />
-              <button type="submit">
-                Cauta
-                <i class="fa fa-search"></i>
-              </button>
-            </form>
+    <div className={`App ${selectedCard ? "card-selected" : ""}`}>
+      {selectedCard && (
+        <EventPopup
+          event={EventsDescription[selectedCard]}
+          onClose={closePopup}
+        />
+      )}
+
+      <div className="App">
+        <header className="App-header">
+          <div class="topnav">
+            <a
+              className={selectedEventType === "all" ? "active" : ""}
+              onClick={() => handleEventTypeClick("all")}
+              href="#home"
+            >
+              Home
+            </a>
+            <a
+              className={selectedEventType === "concert" ? "active" : ""}
+              onClick={() => handleEventTypeClick("concert")}
+              href="#concert"
+            >
+              Concerte
+            </a>
+            <a
+              className={selectedEventType === "conferinta" ? "active" : ""}
+              onClick={() => handleEventTypeClick("conferinta")}
+              href="#conferinta"
+            >
+              Conferinte
+            </a>
+            <div class="search-container">
+              <form action="/action_page.php">
+                <input
+                  type="text"
+                  placeholder="Titlu, oras"
+                  name="search"
+                  value={searchText}
+                  onChange={handleSearch}
+                />
+              </form>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <main className="App-main">
-        <div className="main_1">
-          {filteredEvents.map((event) => (
-            <Card
-              key={event.id}
-              title={event.title}
-              date={event.date}
-              location={event.location}
-              city={event.city}
-              image={event.image}
-              onClick={() => onEventClick(event.id)}
-            />
-          ))}
-        </div>
-        <div className="main_2" ref={main2ContainerRef}>
-          {selectedCard && (
-            <>
-              <img
-                className="selected-card-image"
-                src={EventsDescription[selectedCard].image}
-                alt="Selected Event"
+        <main className="App-main">
+          <div className="main_1">
+            {filteredEvents.map((event) => (
+              <Card
+                key={event.id}
+                title={event.title}
+                date={event.date}
+                location={event.location}
+                city={event.city}
+                image={event.image}
+                onClick={() => onEventClick(event.id)}
               />
-              <h2>Description</h2>
-              {renderParagraphs(EventsDescription[selectedCard].description)}
-              <h2>Details</h2>
-              {renderParagraphs(EventsDescription[selectedCard].details)}
-            </>
-          )}
-        </div>
-      </main>
+            ))}
+          </div>
+        </main>
 
-      <footer className="App-footer">
-        <p>App 2024</p>
-      </footer>
+        <footer className="App-footer">
+          <p>App 2024</p>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Events/EventPopup.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./EventPopup.css";
+
+const EventPopup = ({ event, onClose }) => {
+  const renderParagraphs = (text) => {
+    return text
+      .split("\n")
+      .map((paragraph, index) => <p key={index}>{paragraph}</p>);
+  };
+
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("fullscreen-popup-overlay")) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="fullscreen-popup-overlay" onClick={handleOverlayClick}>
+      <div className="fullscreen-popup">
+        <span className="close-button" onClick={onClose}>
+          X
+        </span>
+        <div className="popup-content">
+          <h2>{event.title}</h2>
+          <img src={event.image} alt="Event Image" />
+          <h2>Description</h2>
+          {renderParagraphs(event.description)}
+          <h2>Details</h2>
+          {renderParagraphs(event.details)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventPopup;
